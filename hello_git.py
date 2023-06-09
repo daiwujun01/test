233f8abf851b04337ba61af99a9110165e756e9b
wujun <wujun@macmini.com>
2023-06-09 10:36:08 +0800

    learned sth new

--- hello_git.py
+++ hello_git.py
@@ -4,4 +4,5 @@ print("Learning how to use git with TWT on Youtube")
 
 print("made a new branch")
 print("weired that merge command does not work")
-
+print("ok now it's working, well first should checkout to the main \
+      and the command should be 'merge new' not 'merge main' ")
