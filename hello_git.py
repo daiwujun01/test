08a1b1b33e79c897d70264e4f10651870fdbf383
wujun <wujun@macmini.com>
2023-06-09 14:09:13 +0800

    2nd test push

--- hello_git.py
+++ hello_git.py
@@ -6,3 +6,5 @@ print("made a new branch")
 print("weired that merge command does not work")
 print("ok now it's working, well first should checkout to the main \
       and the command should be 'merge new' not 'merge main' ")
+
+print("test 2nd push")
